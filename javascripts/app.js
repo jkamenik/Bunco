be8ad29a8778bb86ebea1055e954a547a50eae14
John Kamenik <jkamenik@gmail.com>
2012-11-20 07:17:50 -0500

    Making the events work.

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,22 +6,95 @@ var score_card = {};
       round_number: 0,
       p1_score:     0,
       p2_score:     0,
-      active:       false
+
+      // items set by validations
+      active:       false,
+      complete:     false,
+      player:       1
+    },
+
+    addScore: function(score){
+      if(score == 0){
+        this.togglePlayer();
+      } else {
+        var p       = this.get('player');
+        var p_score = "p"+p+"_score";
+
+        this.set(p_score,(this.get(p_score) + score));
+      }
+    },
+
+    validate: function(attributes){
+      if(attributes['p1_score'] >= 21 || attributes['p2_score'] >= 21){
+        this.set('complete',true);
+        this.set('active',false);
+      }
+    },
+
+    togglePlayer: function(){
+      this.set('player', this.get('player') == 1 ? 2 : 1);
     }
   });
 
   score_card.Rounds = Backbone.Collection.extend({
-    model: score_card.Round
+    model: score_card.Round,
+
+    activeRound: function(){
+      return this.where({active: true})[0];
+    },
+    addScore: function(score){
+      this.activeRound().addScore(score);
+    }
+  });
+
+  score_card.RoundListView = Backbone.View.extend({
+    initialize: function(){
+      this.template = _.template($("#round").html());
+    },
+
+    render: function(){
+      var dom      = this.$el;
+      var template = this.template;
+      dom.empty();
+
+      this.collection.each(function(round){
+        dom.append(template(round.toJSON()));
+      });
+
+      // needed for jquery to rerender dom
+      dom.trigger('create');
+
+      return this;
+    }
   });
 
   score_card.init = function(){
     var rounds = new score_card.Rounds();
 
     _.each([1,2,3,4,5,6],function(num){
-      console.log(rounds);
       rounds.add({round_number: num});
     });
     rounds.at(0).set('active',true);
+
+    var view = new score_card.RoundListView({
+      el:         ":jqmData(role='content') :jqmData(role='collapsible-set')",
+      collection: rounds
+    });
+    view.render();
+
+    rounds.on('change:complete',function(model){
+      if(model.active){return;}
+
+      var index = this.indexOf(model);
+      var next = this.at(index+1);
+
+      if(next){
+        next.set('active',true);
+      }
+    });
+    rounds.on('change',function(model){view.render()});
+
+    score_card.rounds = rounds;
   };
 
   // now add button bindings
@@ -31,5 +104,18 @@ var score_card = {};
 
   $('#score_card').live('pageinit', function(event){
     score_card.init();
+  });
+
+  $('#points_0').live('click',function(){
+    score_card.rounds.addScore(0);
+  });
+  $('#points_1').live('click',function(){
+    score_card.rounds.addScore(1);
+  });
+  $('#points_5').live('click',function(){
+    score_card.rounds.addScore(5);
+  });
+  $('#points_21').live('click',function(){
+    score_card.rounds.addScore(21);
   });
 })(jQuery);
